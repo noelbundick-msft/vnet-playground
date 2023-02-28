670500b4a939868f173769a4318224611846cc63
Noel Bundick <noel.bundick@microsoft.com>
2023-02-28 18:42:38 +0000

    hack: expose container env vars to ssh sessions

--- image/10-start-sshd.sh
+++ image/10-start-sshd.sh
@@ -3,3 +3,7 @@ set -euo pipefail
 
 rc-status
 rc-service sshd start
+
+# HACK: add env vars to /etc/profile
+# https://learn.microsoft.com/en-us/answers/questions/179503/webapp-for-containers-app-settings-not-passed-thro
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)  
